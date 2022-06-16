c8f91c8e0c4ff606069981184bce735532da14bb
RationalNivesh <104433699+RationalNivesh@users.noreply.github.com>
2022-06-16 12:21:53 +0530

    Create Nizmoindian.py

---
+++ Nizmoindian.py
@@ -0,0 +1,10 @@
+#Nimzo-Indian Defense
+board=chess.Board()
+board.push_san("d4")
+board.push_san("Nf6")
+board.push_san("c4")
+board.push_san("e6")
+board.push_san("Nc3")
+board.push_san("Bb4")
+
+board
