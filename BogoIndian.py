da6ed1d8807a70cc5e91d74c383f4434fab2599f
RationalNivesh <104433699+RationalNivesh@users.noreply.github.com>
2022-06-16 12:22:36 +0530

    Create BogoIndian.py

---
+++ BogoIndian.py
@@ -0,0 +1,10 @@
+#Bogo-Indian Defense
+board=chess.Board()
+board.push_san("d4")
+board.push_san("Nf6")
+board.push_san("c4")
+board.push_san("e6")
+board.push_san("Nf3")
+board.push_san("Bb4")
+
+board
