2536d0403f06f285a91f96884ca5abb11e6ef750
RationalNivesh <104433699+RationalNivesh@users.noreply.github.com>
2022-06-16 12:22:18 +0530

    Create queenindian.py

---
+++ queenindian.py
@@ -0,0 +1,10 @@
+#Queen's Indian Defense
+board=chess.Board()
+board.push_san("d4")
+board.push_san("Nf6")
+board.push_san("c4")
+board.push_san("e6")
+board.push_san("Nf3")
+board.push_san("b6")
+
+board
