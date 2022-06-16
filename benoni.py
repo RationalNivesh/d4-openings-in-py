ea23ac90a26d06d65ee7c8c37f6e6cf4170c0f2e
RationalNivesh <104433699+RationalNivesh@users.noreply.github.com>
2022-06-16 12:24:22 +0530

    Create benoni.py

---
+++ benoni.py
@@ -0,0 +1,13 @@
+#Benoni Defense: Modern Variation, 4.Nc3 exd5 5.cxd5 d6
+board=chess.Board()
+board.push_san("d4")
+board.push_san("Nf6")
+board.push_san("c4")
+board.push_san("c5")
+board.push_san("d5")
+board.push_san("e6")
+board.push_san("Nc3")
+board.push_san("exd5")
+board.push_san("cxd5")
+board.push_san("d6")
+board
